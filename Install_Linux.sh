61a74e8d3363674aebc4c50dc62c947328666300
Igor <89133466+igortoxichnyy@users.noreply.github.com>
2021-10-06 05:09:51 +0300

    Add files via upload

---
+++ Install_Linux.sh
@@ -0,0 +1,6 @@
+sudo apt-get install python3
+sudo apt-get install python3-pip
+pip install requests
+rm Install_Win.bat
+rm Run_Win.bat
+rm Install_Linux.sh
---
+++ Install_Win.bat
@@ -0,0 +1,4 @@
+pip install requests
+del Install_Linux.sh
+del Run_Linux.sh
+del Install_Win.bat
